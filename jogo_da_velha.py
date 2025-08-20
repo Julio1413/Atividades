ee35fa9a8eb02a5e22006439816dbdf7b9c42a36
ERROR_404 <125930596+Julio1413@users.noreply.github.com>
2025-08-20 13:00:19 +0000

    coreção string (+)

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,12 +1,13 @@
 import os
-x=[]
-o=[]
-vez ="  X  "
-casas = {"1":"     ","2":"     ","3":"     ","4":"     ","5":"     ","6":"     ","7":"     ","8":"     ","9":"     "}
-combinacoes_vitoria = [[1, 2, 3],[4, 5, 6],[7, 8, 9],[1, 4, 7],[2, 5, 8],[3, 6, 9],[1, 5, 9],[3, 5, 7]]
-def velha():
+x = []
+o = []
+vez = "  X  "
+msg = ''
+casas = {str(i): '     ' for i in range(1, 10)}  # só de 1 a 9
+combinacoes_vitoria = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+def velha(msg=''):
     os.system("clear")
-    print(f"o--Jogo-da-velha--o")
+    print(f"o--Jogo-da-velha--O")
     print(f"|=================|")
     print(f"|{casas['1']}|{casas['2']}|{casas['3']}|")
     print(f"|-----+-----+-----|")
@@ -14,36 +15,35 @@ def velha():
     print(f"|-----+-----+-----|")
     print(f"|{casas['7']}|{casas['8']}|{casas['9']}|")
     print(f"o=================o")
+    if msg:
+        print(msg)
 def venceu():
-        for a,b,c in combinacoes_vitoria:
-            if a in x and b in x and c in x:
-                velha()
-                print("X venceu!")
-                return True
-            elif a in o and b in o and c in o:
-                velha()
-                print("O venceu!")
-                return True
-        if len(x) + len(o) == 9:
-            velha()
-            print("Velha!")
+    for a, b, c in combinacoes_vitoria:
+        if a in x and b in x and c in x:
+            velha("X venceu!")
             return True
-        return False
-while venceu()==False:    
-    velha()
-    jogada = str(input(f"{vez}, escolha uma casa:\n(1-9) "))
-    if not jogada in '123456789':
-        print("Jogada inválida! Escolha um número entre 1 e 9.")
-    else:
+        elif a in o and b in o and c in o:
+            velha("O venceu!")
+            return True
+    if len(x) + len(o) == 9:
+        velha("Velha!")
+        return True
+    return False
+while not venceu():
+    velha(msg)
+    jogada = input(f"{vez}, escolha uma casa (1-9): ")
+    if jogada.isnumeric() and 1 <= int(jogada) <= 9:
         if int(jogada) in x or int(jogada) in o:
-            print("Casa já ocupada! Escolha outra casa.")
+            msg = "Casa já ocupada! Escolha outra."
         else:
             if vez == "  X  ":
                 x.append(int(jogada))
-                casas[f"{int(jogada)}"] = vez
+                casas[jogada] = vez
                 vez = "  O  "
             else:
                 o.append(int(jogada))
-                casas[f"{int(jogada)}"] = vez
+                casas[jogada] = vez
                 vez = "  X  "
-    venceu()
+            msg = ''
+    else:
+        msg = "Jogada inválida! Digite um número de 1 a 9."
