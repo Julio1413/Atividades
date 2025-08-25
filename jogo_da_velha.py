db1668ab7fd1faa396b53c06220349ef71fc8340
ERROR_404 <juliogabryel.jas@gmail.com>
2025-08-25 16:46:11 -0300

    Adiciona medição de tempo e comentários ao jogo da velha (+)
    Inclui importação do módulo time para calcular a duração da partida e exibe o tempo ao final do jogo. Adiciona comentários explicativos ao código, ajusta a limpeza de tela para Windows (os.system('cls')) e melhora a organização do código para maior clareza.

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,12 +1,18 @@
+#iportando funções (os para limpar a tela e time para calcular o tempo de jogo)
 import os
+import time
+#listas dos jogadores
 x = []
 o = []
 vez = "  X  "
 msg = ''
+#dicionário das casas do jogo
 casas = {str(i): '     ' for i in range(1, 10)}
+#combinações de vitória
 combinacoes_vitoria = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+#função para mostrar o tabuleiro
 def velha(msg=''):
-    os.system("clear")
+    os.system("cls")
     print(f"|--Jogo-da-velha--|")
     print(f"|=================|")
     print(f"|{casas['1']}|{casas['2']}|{casas['3']}|")
@@ -17,6 +23,7 @@ def velha(msg=''):
     print(f"|=================|")
     if msg:
         print(msg)
+#função para verificar se alguém venceu e parar o jogo
 def venceu():
     for a, b, c in combinacoes_vitoria:
         if a in x and b in x and c in x:
@@ -29,13 +36,17 @@ def venceu():
         velha("Velha!")
         return True
     return False
+#loop principal do jogo
+inicio = time.time()
 while not venceu():
     velha(msg)
+    #Pedindo a jogada
     jogada = input(f"{vez.replace(' ','')}, escolha uma casa (1-9): ")
+    #verificando se a jogada é valida
     if jogada.isnumeric() and 1 <= int(jogada) <= 9:
-        if int(jogada) in x or int(jogada) in o:
-            msg = "Casa já ocupada! Escolha outra."
+        if int(jogada) in x or int(jogada) in o: msg = "Casa já ocupada! Escolha outra."
         else:
+            #Trocando a vez e marcando a jogada no tabuleiro
             if vez == "  X  ":
                 x.append(int(jogada))
                 casas[jogada] = vez
@@ -45,5 +56,5 @@ while not venceu():
                 casas[jogada] = vez
                 vez = "  X  "
             msg = ''
-    else:
-        msg = "Jogada inválida! Digite um número de 1 a 9."
+    else: msg = "Jogada inválida! Digite um número de 1 a 9."
+print(f'A partida durou {int((time.time() - inicio) // 60)} minuto(s) e {((time.time() - inicio) % 60):.2f} segundo(s).')
