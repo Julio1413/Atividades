cb62c6748ab0023de8b8925d513a7a1ef5e49fbf
ERROR_404 <125930596+Julio1413@users.noreply.github.com>
2025-10-17 14:49:41 -0300

    (+)

--- Quiz/quiz.py
+++ Quiz/quiz.py
@@ -4,16 +4,16 @@ import random
 import time
 import playsound3
 
-with open('perguntas.txt','r') as file:
+with open('perguntas.txt','r', encoding="utf-8") as file:
     perguntas = file.readlines()
-with open('respostas.txt','r') as file:
+with open('respostas.txt','r', encoding="utf-8") as file:
     respostas = file.readlines()
-print (perguntas)
-print (respostas)
-
-def resultados(acertos):
-    pass
-def main(page: ft.Page):
+    
+def perguntas(page):
+    page.clean()
+    page.update()
+    def resultados(acertos):
+        pass
     def verificar_resposta():
         pass
     def acertou():
@@ -23,13 +23,65 @@ def main(page: ft.Page):
     def proxima_pergunta(e):
         pass
     acertos=0
+    
+def main(page: ft.Page):
     page.title = "Quiz 6X2"
     page.bgcolor = ft.Colors.LIGHT_BLUE
     page.theme_mode = ft.ThemeMode.LIGHT
     page.update()
     
-    
-    #Construção da página
-    
+    page.add(
+        ft.Column(controls=[ft.Container(height=90,
+        content=ft.Container(alignment=ft.alignment.bottom_center,
+            padding=ft.padding.only(left=10, right=10,bottom=10),
+            blur=(10,10),
+            content=ft.Row(
+                controls=[
+                    ft.Icon(name=ft.Icons.QUIZ_ROUNDED, color=ft.Colors.WHITE,size=30),
+                    ft.Text(
+                        value='Quiz 6X2',
+                        color=ft.Colors.WHITE,
+                        size=20,
+                        weight=ft.FontWeight.BOLD,
+                    ),
+                    ft.Icon(name=ft.Icons.QUIZ_ROUNDED, color=ft.Colors.WHITE,size=30),
+                ],
+                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            ),
+            height=50,
+        ),
+    ),
+    ft.Text(f'\n',size=1),
+                               ])
+    )
+    page.add(
+        ft.Container(
+            expand=True,
+            padding=ft.padding.all(10),
+            content=ft.ElevatedButton(
+                expand=True,
+                on_click=lambda e: perguntas(page),
+                style=ft.ButtonStyle(
+                    shape=ft.RoundedRectangleBorder(radius=20),
+                    bgcolor=ft.colors.BLUE_600,
+                    color=ft.colors.WHITE,
+                    elevation=5,
+                    overlay_color=ft.colors.BLUE_200,
+                ),
+                content=ft.Container(
+                    margin=ft.margin.only(top=10, bottom=10),
+                    expand=True,
+                    alignment=ft.alignment.center,
+                    content=ft.Column(
+                        controls=[
+                            ft.Text('Iniciar Quiz', size=20, weight=ft.FontWeight.BOLD),
+                            ft.Text('10 perguntas aleatórias sobre Python', size=20, weight=ft.FontWeight.BOLD)
+                        ]
+                    )
+                )
+            )
+        )
+    )
+        
     
 ft.app(target=main)
